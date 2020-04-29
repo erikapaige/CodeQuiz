2d41d0514c34d3f27cb3a160511d394441f2c83b
Erika Paige <erika.paige.handley@gmail.com>
2020-04-29 01:43:54 -0700

    attempt 2 trying to get quiz to work

--- score.js
+++ score.js
@@ -1,38 +1,61 @@
+// calling the elements
+const start = document.getElementById("start-btn")
+const quiz = document.getElementById("quiz")
+const question = document.getElementById("question")
+const answerA = document.getElementById("A")
+const answerB = document.getElementById("B")
+const answerC = document.getElementById("C")
+const answerD = document.getElementById("D")
+const counter = document.getElementById("counter")
+const timeGage = document.getElementById("timeGage")
+const progress = document.getElementById("progress")
+const scoreDiv = document.getElementById("scoreContainer")
 
-// starting timer
-let Timer = 60
-// starting question / quiz index
-let question = 0
-// starting score
-let score = 0
 //array of quiz questions
   const quiz = [
-      {
-        qustion: 'Where do the tags for JavaScript get inserted on the HTML page?',
-        answer: 'the <head> and the <body> sections',
-        option: ['<head>', '<body>', '<footer>', 'the <head> and the <body> sections']
-      },
-      {
-        qustion: 'What HTML tag is used for JavaScript?',
-        answer: '<javascript>',
-        option: ['<js>', '<javascript>', '<script>', '<scripting>']
-      },
-      {
-        qustion: 'A JavaScript variable is written:',
-        answer: 'var personName',
-        option: ['var personName', 'variable personname', 'var personname', 'v personName']
-      },
-      {
-        qustion: 'How are comments added in a JavaScript?',
-        answer: '//Comment//',
-        option: ['${Commnet}', '//Comment//', '<==Comment ==>', '!= Comment !==']
-      },
-      {
-        qustion: 'Arrays are a....',
-        answer: 'Collection of data', 
-        option: ['Collection of commands', 'Collection of data', 'Collection of HTML tags', 'Collection of images']
-      },
+    {
+      question: "What is JavaScript used for?",
+      answerA: "To create responsive and interactive elements for webpages",
+      answerB: "To visually style the page, including: colors, layouts and fonts",
+      answerC: "To create pages that are dispalyed on the World Wide Web",
+      answerD: "To define the content of web pages",
+      correct: "A"
+    },
+    {
+      question: "What HTML tag is used for JavaScript?",
+      answerA: "<javascript>",
+      answerB: "<js>",
+      answerC: "<script>",
+      answerD: "<scripting>",
+      correct: "C"
+    },
+    {
+      question: "How are comments added in a JavaScript?",
+      answerA: "${Commnet}",
+      answerB: "//Comment//",
+      answerC: "<==Comment ==>",
+      answerD: "!= Comment !==",
+      correct: "B"
+    },
+    {
+      question: "Arrays are a....",
+      answerA: "Collection of commands",
+      answerB: "Collection of data",
+      answerC: "Collection of HTML tags",
+      answerD: "Collection of images",
+      correct: "B"
+    },
+    {
+      question: "A JavaScript variable is written:",
+      answerA: "v personName",
+      answerB: "var personname",
+      answerC: "variable personname",
+      answerD: "var personName",
+      correct: "D"
+    }
     ]
+
+//
 
 //turn the array in to buttons
 function arrayButtons() {
